77414ada6eb154a20b0c091563d78f69d096d4e2
Ali Jowhar <alijowharcoding@gmail.com>
2023-03-23 18:17:36 -0600

    small changes to the styling container and some js

---
+++ main.js
@@ -0,0 +1,14 @@
+let pokemonCount = 151;
+
+// {{name: bulbasuar, type:[],}}
+const pokedex = new Map([]);
+
+window.onload = function () {
+  getPokemon(i);
+};
+
+function getPokemon() {
+  let url = "https://pokeapi.co/api/v2/pokemon/1";
+}
+
+// updateDisplay
