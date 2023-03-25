577a4649ce50a720f1d01ebcbff52ecdc1a39bd5
Ali Jowhar <alijowharcoding@gmail.com>
2023-03-25 01:52:46 -0600

    wrote some logic for main.js, on fetching from poke api and storing

--- main.js
+++ main.js
@@ -4,16 +4,31 @@ let pokemonCount = 151;
 const pokedex = new Map([]);
 
 window.onload = async function () {
-  getPokemon();
+  getPokemon(65);
 };
 
-async function getPokemon() {
-  let url = "https://pokeapi.co/api/v2/pokemon/1";
+async function getPokemon(num) {
+  let url = "https://pokeapi.co/api/v2/pokemon/" + num;
   let response = await fetch(url);
   let pokemon = await response.json();
 
-  console.log(pokemon);
+  let name = pokemon.name;
+  let type = pokemon.types;
+
+  let res = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + num);
+  let desc = await res.json();
+
+  desc = desc["flavor_text_entries"]["5"]["flavor_text"];
+
+  pokedex.set({
+    pkmnName: name,
+  });
+
+  console.log(pokedex);
 }
 getPokemon;
 
 // updateDisplay
+console.log(pokedex);
+// pkmnType: type,
+// pkmnDesc: desc,
