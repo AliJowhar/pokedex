2948d85dfe473d760c7dece53b0e39d757d3dbb7
Ali Jowhar <alijowharcoding@gmail.com>
2023-03-25 02:49:08 -0600

    added name desc type and img of pokemon

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let pokemonCount = 151;
 
 // {{name: bulbasuar, type:[],}}
-const pokedex = new Map([]);
+const pokedex = new Map();
 
 window.onload = async function () {
-  getPokemon(65);
+  getPokemon(1);
 };
 
 async function getPokemon(num) {
@@ -12,23 +12,20 @@ async function getPokemon(num) {
   let response = await fetch(url);
   let pokemon = await response.json();
 
-  let name = pokemon.name;
-  let type = pokemon.types;
+  let name = pokemon["name"];
+  let types = pokemon["types"];
+  let pkmnImg = pokemon["sprites"]["front_shiny"];
 
-  let res = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + num);
+  let res = await fetch(pokemon["species"]["url"]);
   let desc = await res.json();
 
   desc = desc["flavor_text_entries"]["5"]["flavor_text"];
-
-  pokedex.set({
-    pkmnName: name,
-  });
-
-  console.log(pokedex);
+  pokedex[num] = {
+    name: name,
+    types: types,
+    img: pkmnImg,
+    desc: desc,
+  };
 }
-getPokemon;
 
 // updateDisplay
-console.log(pokedex);
-// pkmnType: type,
-// pkmnDesc: desc,
