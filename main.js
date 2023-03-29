ea40fa31755c3147b8de7aeb7fe137f27e37843a
Ali Jowhar <alijowharcoding@gmail.com>
2023-03-29 00:21:45 -0600

    fixed the var names in getPokemon function

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
 let pokemonCount = 151;
 
 // {{name: bulbasuar, type:[],}}
-const pokedex = new Map();
+const pokedex = {};
 
 window.onload = async function () {
-  getPokemon(1);
+  for (let i = 1; i < pokemonCount; i++) {
+    await getPokemon(i);
+    // console.log(pokedex[i]);
+  }
+
+  // console.log(pokedex);
 };
 
 async function getPokemon(num) {
-  let url = "https://pokeapi.co/api/v2/pokemon/" + num;
+  let url = "https://pokeapi.co/api/v2/pokemon/" + num.toString();
   let response = await fetch(url);
   let pokemon = await response.json();
 
-  let name = pokemon["name"];
-  let types = pokemon["types"];
-  let pkmnImg = pokemon["sprites"]["front_shiny"];
+  let pokemonName = pokemon["name"];
+  let pokemonType = pokemon["types"];
+  let pokemonImg = pokemon["sprites"]["front_shiny"];
 
   let res = await fetch(pokemon["species"]["url"]);
   let desc = await res.json();
 
-  desc = desc["flavor_text_entries"]["5"]["flavor_text"];
+  // console.log(types);
+
+  pokemonDesc = desc["flavor_text_entries"]["5"]["flavor_text"];
+
   pokedex[num] = {
-    name: name,
-    types: types,
-    img: pkmnImg,
-    desc: desc,
+    name: pokemonName,
+    img: pokemonImg,
+    types: pokemonType,
+    desc: pokemonDesc,
   };
 }
 
